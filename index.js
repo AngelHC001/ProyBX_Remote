e153809a01e993b1a3167d87a46a76341cf6dd09
AngelHC <devcandy3469@outlook.com>
2026-06-24 11:04:14 -0600

    production attempt 1

--- index.js
+++ index.js
@@ -5,22 +5,32 @@ import express from 'express';
 import process from 'process';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 import authRoutes from './backend/auth_routes.js';
 import driveRoutes from './backend/drive_routes.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //Middleware globales
-app.use(cors({origin: ['http://localhost:5173', 'http://localhost:4173'], credentials: true}));
+app.use(cors({origin: 'https://proybx-remote.onrender.com', credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
 //Enrutamiento interno
 app.use('/auth',authRoutes);
 app.use('/drive',driveRoutes);
+app.use(express.static(path.join(__dirname,'dist')))
 
-//const port = process.env.PORT || 8080;
-//app.listen(port, () => console.log(`Servidor en puerto ${port}`));
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'dist','index.html'));
+})
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Servidor en puerto ${port}`));
 
 export const uploadAuditoria = app;
