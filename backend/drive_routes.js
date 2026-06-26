f7ccaf6eddf641c37f98e3034240b4f6b0f6814c
AngelHC <devcandy3469@outlook.com>
2026-06-26 12:57:49 -0600

    Pivot stable

--- backend/drive_routes.js
+++ backend/drive_routes.js
@@ -1,15 +1,71 @@
 import express from 'express';
 import process from 'process';
+import { google } from "googleapis";
 import busboy from "busboy";
+import { PassThrough } from "stream";
+import { Buffer } from "buffer";
 
-import {sanitizeText} from '../backend/utils.js';
+import {sanitizeText} from './utils.js';
+
 const router = express.Router();
 
-//const drive = useDrive;
-//import { useDrive } from './drive_config.js';
-
+//Variables de entorno en produccion
 const DRIVE_FOLDER = process.env.VITE_DRIVE_FOLDER;
+const DRIVE_FILE = process.env.VITE_DRIVE_FILE;
 
+//oauth
+const OAUTH_ID = process.env.VITE_OAUTH_ID;
+const SECRET = process.env.VITE_SECRET_KEY;
+const RFK = process.env.VITE_REFRESH_TOKEN;
+
+const REDIRECT_URI = process.env.VITE_REDIRECT_URI;
+
+//AUTENTICACION USANDO EL CLIENT ID
+const auth2Client = new google.auth.OAuth2({
+    clientId: OAUTH_ID,
+    clientSecret:SECRET,
+    redirectUri: REDIRECT_URI
+});
+
+auth2Client.setCredentials({ refresh_token: RFK });
+
+//EVENTOS DE CAMBIO
+auth2Client.on('tokens',(tokens) => {
+    if(tokens.refresh_token){
+        console.log('Nuevo token');
+    }
+    console.log('Acceso temporal renovado');
+});
+
+//USA EL CLIENT ID
+const drive = google.drive({
+    version: "v3", 
+    auth: auth2Client
+});
+
+
+//FUNCION PARA EXTRAER EXCLUSIVE.JSON DEL DRIVE
+let usuariosCache = null;
+let ultimaCarga = 0;
+const CACHE_TIME = 1000 * 60 * 5; //5 minutos de cache
+
+export async function getUsersFromDrive(){
+    // Si han pasado menos de 5 minutos, retorna la caché
+    if(usuariosCache && (Date.now() - ultimaCarga < CACHE_TIME)){
+        return usuariosCache;
+    }
+
+    //Si no, vuelve a consultar
+    const response = await drive.files.get({
+        fileId: DRIVE_FILE,
+        alt: 'media'
+    });
+
+    //El json
+    usuariosCache = response.data;
+    ultimaCarga = Date.now();
+    return usuariosCache;
+}
 
 router.post('/upload', async(req,res) => {
     //CONFIGURACION DE CORS
@@ -19,9 +75,11 @@ router.post('/upload', async(req,res) => {
     //VERIFICACION DE CREDENCIALES
     try {
         const tokenResponse = await auth2Client.getAccessToken();
+        
         if(!tokenResponse.token){
             throw new Error("NO SE PUDO GENERAR UN TOKEN ACCESS VALIDO");
         }
+        
         console.log("¡Autenticación OAUTH exitoso!");
     } catch (authError) {
         console.error("Error crítico de autenticación:", authError.message);
@@ -45,36 +103,44 @@ router.post('/upload', async(req,res) => {
     }
 
     console.log('PASO FILTROS');
+    
+    //AQUI SE DETIENE EN PRODUCCION
 
-    try {
-        //INICIALIZAR        
+    try {        
         const bb = busboy({ headers: req.headers });
         const fields = {};
-        const fileQueue = { caja:[], alcn:[], sala:[], edif: [] };
+        const filesToUpload = [];
 
-        //2. PROCESAR TEXTO DEL FORMULARIO
-        bb.on('field', (fieldname,val) => { fields[fieldname] = val; });             
-        
-        //3. SUBIR IMAGENES A SU CARPETA - STREAMING DIRECTO
+        //PROCESAMOS DATOS DEL FORMULARIO
+        bb.on('field', (fieldname,val) => {
+           fields[fieldname] = val; 
+        });
+
+        //PROCESAR IMAGENES
         bb.on('file', (fieldname,file,info) => {
-            const section = Object.keys(fileQueue).find(s => s.fieldname.startsWith(s));
-            if(section){
-                fileQueue[section].push({file,info});
-            }
+            const { filename, mimeType } = info;
+            const buffers = [];
 
+            file.on('data', (data) => buffers.push(data));
+            file.on('end', () => {
+                filesToUpload.push({
+                    name: filename,
+                    mimeType: mimeType,
+                    content: Buffer.concat(buffers)
+                })
+            });
         }); 
-        console.log('IMAGENES PROCESADAS EN COLA');
-       
+
+        console.log('PASO EL PROCESS');
         bb.on('finish', async() => {
             try {
-                //ZERO - EXTRAER DATOS ENVIADOS
                 const metadata = JSON.parse(fields.metadata || '{}');
                 const secciones = JSON.parse(fields.secciones || '{}');
 
-                //1. CREAR CARPETA
+                //Crear carpeta
                 const hoy = new Date().toISOString().split('T')[0];
                 const folderName = `${metadata.sucursal?.replace(/\s+/g, '_')}_${hoy}`;
-
+                
                 const folderResponse = await drive.files.create({
                     requestBody: { 
                         name: folderName,
@@ -83,10 +149,12 @@ router.post('/upload', async(req,res) => {
                     },
                     fields: 'id'
                 });
-                    
+            
                 const folderId = folderResponse.data.id;
-                console.log('FOLDER CREADO');
 
+                console.log('FOLDER CREADO');
+                
+                //Crear y subir archivo de texto plano (.txt)
                 let contenidoTxt = `REPORTE: ${metadata.sucursal}\nFECHA: ${metadata.fecha}\n\n`;
                 Object.keys(secciones).forEach((seccion) => {
                     contenidoTxt += `\n[${seccion.toUpperCase()}]\n`;
@@ -99,35 +167,46 @@ router.post('/upload', async(req,res) => {
                     });
                 });
 
-                console.log('TEXTO PROCESADO');
-
                 await drive.files.create({
-                    requestBody: { name: 'reporte_datos.txt', parents: [folderId]},
-                    media: { mimeType: 'text/plain', body: contenidoTxt }
-                });
-                console.log('TEXTO SUBIDO');
-
-                for (const section in fileQueue) {
-                    if(fileQueue[section].length > 0){
-                        console.log('SUBIENDO ARCHIVOS SECCION '+ section);
-                        await Promise.all(fileQueue[section].map(f => 
-                            drive.files.create({
-                                requestBody: { name: f.info.filename, parents:[folderId] },
-                                media: { mimeType: f.info.mimeType, body: f.file }
-                            })
-                        ));
+                    requestBody: { 
+                        name: 'reporte_datos.txt', 
+                        parents: [folderId]
+                    },
+                    media: { 
+                        mimeType: 'text/plain', 
+                        body: contenidoTxt 
                     }
-                }
-                console.log('IMAGENES SUBIDAS');
+                });
+
+                console.log('TEXTO PLANO LISTO');
                 
-                return res.status(200).json({status: 'success'});
+
+                //Subir las imágenes asociadas vinculándolas a la carpeta
+                for(const fileObj of filesToUpload){
+                    const bufferStream = new PassThrough();
+                    bufferStream.end(fileObj.content);
+                    
+                    await drive.files.create({
+                        requestBody: {
+                            name: fileObj.name, 
+                            parents:[folderId]
+                        },
+                        media: {
+                            mimeType: fileObj.mimeType, 
+                            body: bufferStream
+                        }
+                    });
+                }
+
+                console.log('IMAGENES LISTAS');
+                return res.status(200).json({status: 'success', message: 'Sincronizado con exito'});
             } catch (error) {
                 console.error(error);
                 return res.status(500).json({ error: 'Error al procesar la subida a Drive: ' + error.message });
             }
         });
 
-        req.pipe(bb);
+        req.pipe(bb);   
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
